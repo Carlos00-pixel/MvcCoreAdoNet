1bee0c6b9c81fc6c6a01e32faa0b6029d6f9a81f
Carlos00-pixel <carlos.mayoral00@gmail.com>
2023-02-07 14:23:38 +0100

    Add project files.

---
+++ MvcCoreAdoNet/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreAdoNet.Models;
+
+namespace MvcCoreAdoNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult VistaMascota()
+        {
+            Mascota mascota = new Mascota();
+            mascota.Nombre = "Animal";
+            mascota.Raza = "Tu Mascota";
+            mascota.Peso = 22;
+            return View(mascota);
+        }
+
+    }
+}
